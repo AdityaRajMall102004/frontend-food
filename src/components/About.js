f51b629097f6518854fb0720b9bb5da43839779b
Aditya Raj Mall <adityarajmall102004@gmail.com>
2025-08-22 01:27:09 +0530

    Updated project code

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
-import thal from "../Photos/thali1.jpg";
 import { Link } from "react-router-dom";
+const linkedinUrl = process.env.REACT_APP_LINKEDIN_URL;
+ const thaliImg = process.env.REACT_APP_THALI_IMG;
 const About = () =>{
     return (
         <>
           <div className="flex items-center justify-center border h-[630px]">
             <div className="w-[800px]">
             <div>
-              <img className="rounded-full h-2/4 w-3/4" src={thal} />
+              <img src={thaliImg} alt="Thali" className="rounded-full h-2/4 w-3/4" />
             </div>
-              
             </div>
             <div>
             <h1 className="font-bold text-4xl text-gray-600 border mr-5 w-[600px] p-5 bg-blue-100 rounded-md">
@@ -18,14 +18,12 @@ const About = () =>{
               <div className="flex ml-60  items-center flex-col w-72 justify-evenly">
                 <div className="flex mt-10">
                   <h2 className="font-semibold">Created By :-</h2>
-                  <Link to="https://www.linkedin.com/in/aditya-raj-mall-a34aa1281/" className=" ml-2 font-bold text-blue-900">
+                  <Link to={linkedinUrl} className=" ml-2 font-bold text-blue-900">
                     Aditya Raj Mall
                   </Link>
                 </div>
               </div>
-            </div>
-            
-              
+            </div>        
           </div>
           
         </>
